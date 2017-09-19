7d50bf5da3840a176f8bf221980400ab7b1e8158
Péter Biró <biro.peter.hu@gmail.com>
2017-09-19 10:56:14 +0200

    works with input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
+#include <string>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "What is your name?" <<std::endl;
+    std:: string name;
+    std::cin >> name;
+    std::cout << "Hello, "<< name << "!" << std::endl;
     return 0;
 }
