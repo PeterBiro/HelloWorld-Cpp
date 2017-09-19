2e853b238d84514c5a63f9c1b51152f2890424e1
Péter Biró <biro.peter.hu@gmail.com>
2017-09-19 10:14:51 +0200

    without param Hello, World! us written

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++ ){
-        std::cout << "Hello, " << argv[i] << "!" << std::endl;
+    if (argc == 1) {
+        std::cout << "Hello, World!" << std::endl;
+    } else {
+        for (int i = 1; i < argc; i++ ){
+            std::cout << "Hello, " << argv[i] << "!" << std::endl;
+        }
     }
     return 0;
 }
