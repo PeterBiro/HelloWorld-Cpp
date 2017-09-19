106a0f208ce8dce2cd859d36542a7b4ebdbb5d20
Péter Biró <biro.peter.hu@gmail.com>
2017-09-19 10:07:34 +0200

    iterates through args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, " << argv[1] << "!" << std::endl;
+    for (int i = 1; i < argc; i++ ){
+        std::cout << "Hello, " << argv[i] << "!" << std::endl;
+    }
     return 0;
 }
